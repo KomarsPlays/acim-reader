af24bbb078eb8ceea499e5e3d305f328560d8c71
KomarsPlays <Plays@github.com>
2026-03-17 15:44:48 +0300

    add PWA support

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'acim-reader-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/lessons.json',
+  '/manifest.json'
+];
+
+// Установка Service Worker и кэширование файлов
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэш открыт');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация и удаление старых кэшей
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов (работа в офлайне)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Возвращаем из кэша, если есть
+        if (response) {
+          return response;
+        }
+        // Иначе делаем реальный запрос
+        return fetch(event.request);
+      })
+  );
+});
